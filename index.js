51dda1f7d6fbf19d17545f0a9895ac2dc5b1ac3a
shivam123-dev <ss2249@srmist.edu.in>
2021-08-31 09:58:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.post-container').owlCarousel({
+        pagination: false,
+        autoplay:true,
+        responsive: {
+           0:{
+               items:1
+           },
+           700:{
+               items:2
+           },
+           1000:{
+               items:3
+           },
+           1200:{
+               items:4
+           },
+        },
+        loop: true
+    })
+})
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
