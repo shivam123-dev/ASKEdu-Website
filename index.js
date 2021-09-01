bfa45a104f3b2d117f824d25bc7c1e3d442a6136
shivam123-dev <ss2249@srmist.edu.in>
2021-09-01 13:04:11 +0530

    added preloader

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-$(document).ready(function () {
-    $('.post-container').owlCarousel({
-        pagination: false,
-        autoplay:true,
-        responsive: {
-           0:{
-               items:1
-           },
-           700:{
-               items:2
-           },
-           1000:{
-               items:3
-           },
-           1200:{
-               items:4
-           },
-        },
-        loop: true
-    })
+const loader = document.getElementById('preloader')
+window.addEventListener('load', function () {
+    loader.style.display = "none"
 })
 
 const navLinks = document.querySelectorAll('.nav-item')
@@ -26,3 +9,27 @@ const bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
+
+
+$(document).ready(function () {
+    $('.post-container').owlCarousel({
+        pagination: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            },
+        },
+        loop: true
+    })
+})
+
